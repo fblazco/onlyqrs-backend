311f50d2b30615decbdcfb58a46f023baa6545f0
fblazco <66034189+fblazco@users.noreply.github.com>
2026-06-21 22:36:09 -0400

    sad

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 // src/main.js
 const express = require('express');
-const cors = require('cors'); // <-- 1. Descomentado
+//const cors = require('cors'); // <-- 1. Descomentado
 
 const scannerRoutes = require('./routes/scanner.routes.js');
 
@@ -8,7 +8,7 @@ const app = express();
 // <-- 2. Puerto dinámico para Render, o 3000 en local
 const PORT = process.env.PORT || 3000; 
 
-app.use(cors()); // <-- 3. Descomentado (Permite peticiones externas)
+//app.use(cors()); // <-- 3. Descomentado (Permite peticiones externas)
 app.use(express.json()); 
 
 app.use('/api/scanner', scannerRoutes);
